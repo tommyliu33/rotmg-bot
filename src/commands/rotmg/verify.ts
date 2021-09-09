d0cb8f898fbe0f6a716efe92f940035bbbd2e420
tommy liu <86510873+tommyliu33@users.noreply.github.com>
2021-09-09 12:27:21 -0700

    feat: manual method verification

--- src/commands/rotmg/verify.ts
+++ src/commands/rotmg/verify.ts
@@ -1,6 +1,7 @@
 import { Command, command, CommandContext } from "@lib";
 import { ApplicationCommandOptionType } from "discord-api-types/v9";
-import { MessageActionRow, MessageEmbed, MessageSelectMenu } from "discord.js";
+import { verifyMember, VerificationStatus } from "@verification";
+import { Guild, GuildMember } from "discord.js";
 
 @command({
   name: "verify",
@@ -16,74 +17,29 @@ import { MessageActionRow, MessageEmbed, MessageSelectMenu } from "discord.js";
 })
 export default class extends Command {
   public async exec(ctx: CommandContext) {
-    await ctx.interaction.deferReply();
+    const name = ctx.interaction.options.getString("name");
+    const verified = await verifyMember(
+      ctx.interaction.member as GuildMember,
+      ctx.interaction.guild as Guild,
+      name as string
+    );
 
-    const name = ctx.interaction?.options?.getString("name");
-
-    const { client } = ctx;
-    const users = await client.users_db.all();
-
-    const to_verify = users.find((c) => c.ID === ctx.user.id);
-
-    // TODO: most responses should be ephemeral
-    if (name) {
-      if (users.find((c) => c.data.names.includes(name))) {
-        await ctx.interaction.editReply(
-          `You are already verified as \`${name}\`.`
-        );
-      } else {
-        /* user exists and has given a name */
-        if (to_verify) {
-          await ctx.interaction.editReply(
-            `you are not verified under ${name}.
-          \nill do it for you`
-          );
-
-          await client.users_db.push(ctx.user.id, name, "names");
-        } else {
-          console.log(`${ctx.user.tag} does not exist`);
-          await ctx;
-        }
-      }
-    } else {
-      /* api error if there are duplicate names */
-      const names = to_verify?.data?.names!;
-
-      if (names) {
-        const menu = new MessageActionRow().addComponents(
-          new MessageSelectMenu()
-            .setCustomId("select-ign-to-verify")
-            .addOptions(names.map((c) => ({ label: c, value: c })))
-        );
-
-        const embed = new MessageEmbed().setDescription(
-          "Select a name to verify as."
-        );
-
-        await ctx.interaction.editReply({
-          embeds: [embed],
-          components: [menu],
-        });
-      }
-
-      // TODO: goto dms and setup
-      if (!to_verify) {
-        await ctx.interaction.editReply("you are here.");
-
-        // this doesnt work
-        const dm_channel = await ctx.user.createDM();
-        const collected = await dm_channel.awaitMessages();
-
-        await dm_channel.send(
-          "Starting your verification process. Enter your `In-game name` below."
-        );
-
-        if (collected.size) {
-          console.log("we received messages", collected);
-        } else {
-          console.log("they did not do anything.", collected);
-        }
-      }
+    let msg = "";
+    switch (verified) {
+      case -1:
+        msg = "The server's setup is invalid.";
+        break;
+      case 0:
+        msg = "An error occured while trying to verify you.";
+        break;
+      case 1:
+        msg = "You have been successfully verified!";
+        break;
+      case 2:
+        msg = "You are already verified";
+        break;
     }
+
+    await ctx.reply({ content: msg, ephemeral: true });
   }
 }
--- src/verification/verifyMember.ts
+++ src/verification/verifyMember.ts
@@ -2,6 +2,16 @@ import { Bot } from "@lib";
 import type { APIInteractionGuildMember } from "discord-api-types/v9";
 import { Guild, GuildMember } from "discord.js";
 
+export enum VerificationStatus {
+  // bad
+  FAILED = 0,
+  INVALID_SETUP = -1,
+
+  // good
+  SUCCESSFUL = 1,
+  ALREADY_VERIFIED = 2,
+}
+
 /**
  * verify a raider
  * @param member member to verify
@@ -11,23 +21,69 @@ export async function verifyMember(
   member: APIInteractionGuildMember | GuildMember,
   guild: Guild,
   name?: string
-) {
+): Promise<VerificationStatus> {
   const client = guild.client as Bot;
+
+  const users = await client.users_db.all();
+  const target = users.find((c) => c.ID === member.user.id);
+
+  /* a name to verify is given */
+  if (name) {
+    /* user already has the nick set */
+    if (users.find((c) => c.data.names.includes(name))) {
+      return VerificationStatus.ALREADY_VERIFIED;
+    } else {
+      /* if they provided a name not saved in db */
+      if (target) {
+        await client.users_db.push(member.user.id, name, "names");
+      } else {
+        /* first time verifying and they provide a name */
+        await client.users_db.set(member.user.id, { names: [] });
+        await client.users_db.push(member.user.id, name, "names");
+      }
+    }
+  } else {
+    /*
+       const names = to_verify?.data?.names!;
+  
+       if (names) {
+         const menu = new MessageActionRow().addComponents(
+           new MessageSelectMenu()
+             .setCustomId("select-ign-to-verify")
+             .addOptions(names.map((c) => ({ label: c, value: c })))
+         );
+  
+         const embed = new MessageEmbed().setDescription(
+           "Select a name to verify as."
+         );
+  
+         await ctx.interaction.editReply({
+           embeds: [embed],
+           components: [menu],
+         });
+       }
+  
+       // TODO: goto dms and setup
+       if (!to_verify) {
+         await ctx.interaction.editReply("you are here.");
+       }
+      */
+  }
 
   const role = (await client.guilds_db.get(
     guild.id,
     "verified_role"
   )) as string;
 
-  if (!(member instanceof GuildMember))
-    member = await guild.members.fetch(member.user.id);
+  if (!role) return VerificationStatus.INVALID_SETUP;
 
-  if (member.roles.cache.has(role)) {
-    await member.setNickname(`${member.nickname} | ${name}`);
-    return true;
+  try {
+    if (!(member instanceof GuildMember))
+      member = await guild.members.fetch(member.user.id);
+    await member.roles.add(role);
+    await member.setNickname(name as string);
+    return VerificationStatus.SUCCESSFUL;
+  } catch (e) {
+    throw e;
   }
-
-  await member.roles.add(role);
-
-  return true;
 }
