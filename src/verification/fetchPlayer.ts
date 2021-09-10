7041cad5335c9e57ff330c325334f325194fd659
tommy liu <86510873+tommyliu33@users.noreply.github.com>
2021-09-09 23:41:47 -0700

    fix: api wrong search params

--- src/verification/fetchPlayer.ts
+++ src/verification/fetchPlayer.ts
@@ -1,10 +1,11 @@
 import { Player } from "@lib";
-import fetch from "petitio";
+import petitio from "petitio";
 
-const base = "https://nightfirec.at/realmeye-api/";
+const base = "https://nightfirec.at/realmeye-api/?player=";
 
 export async function fetchPlayer(name: string): Promise<Player | null> {
-  const req = await fetch(`${base}?=${name}`).send();
+  console.log(`[fetching] name: ${name}`);
+  const req = await petitio(`${base}${name}`).send();
 
   if (req.statusCode === 200) {
     const json = await req.json();
