7685d67f35bbb622b75a27a5f0e0f92c93ca2f20
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2021-12-12 20:21:04 -0800

    chore: remove extra space

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -24,7 +24,7 @@ export default class implements Event {
         void command.execute(interaction);
 
         logger.info(
-          `${interaction.user.tag} (${interaction.user.id}) ran an command: ${commandName} `
+          `${interaction.user.tag} (${interaction.user.id}) ran an command: ${commandName}`
         );
       } catch (e) {
         const err = e as Error;
