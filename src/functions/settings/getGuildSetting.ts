8f8344df169a5b8ad5ab5178d2e1329c340d2991
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2021-12-17 23:57:12 -0800

    chore: implement new guild settings

--- src/functions/settings/getGuildSetting.ts
+++ src/functions/settings/getGuildSetting.ts
@@ -15,6 +15,9 @@ export enum SettingsKey {
 
   RaidLeaderRole = "raid_leader_role_id",
   VetRaidLeaderRole = "vet_raid_leader_role_id",
+
+  SuspendRole = "suspend_role_id",
+  LogChannel = "log_channel_id",
 }
 
 export async function getGuildSetting(
@@ -23,15 +26,38 @@ export async function getGuildSetting(
 ): Promise<Snowflake> {
   const prisma = container.resolve<PrismaClient>(kPrisma);
 
-  const select = {};
-  Reflect.set(select, key, true);
+  let data = await prisma.guilds
+    .findFirst({
+      where: {
+        id_: guildId,
+      },
+    })
+    .catch(() => {
+      console.log("no guild");
+    });
 
-  const data = await prisma.guilds.findFirst({
-    where: {
-      id_: guildId,
-    },
-    select,
-  });
+  if (!data) {
+    data = await prisma.guilds.create({
+      data: {
+        id_: guildId,
 
-  return Reflect.get(data!, key);
+        afk_check_channel_id: "",
+        vet_afk_check_channel_id: "",
+
+        main_section_id: "",
+        veteran_section_id: "",
+
+        verified_role_id: "",
+        veteran_role_id: "",
+
+        raid_leader_role_id: "",
+        vet_raid_leader_role_id: "",
+
+        log_channel_id: "",
+        suspend_role_id: "",
+      },
+    });
+  }
+
+  return data[key];
 }
