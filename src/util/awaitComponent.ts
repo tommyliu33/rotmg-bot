f19fd87d0ef063f4cfe1484f629cc2107b643e02
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2021-12-23 23:08:01 -0800

    chore(Util): properly type awaitComponent

--- src/util/awaitComponent.ts
+++ src/util/awaitComponent.ts
@@ -1,34 +1,42 @@
-import { APIMessage } from "discord-api-types/v9";
+import type { APIMessage } from 'discord-api-types/v9';
 import type {
-  AwaitMessageCollectorOptionsParams,
-  Client,
-  InteractionExtractor,
-  Message,
-  MessageComponentType,
-} from "discord.js";
-import { Constants, InteractionCollector } from "discord.js"; // eslint-disable-line no-duplicate-imports
+	AwaitMessageCollectorOptionsParams,
+	Client,
+	CollectorFilter,
+	Interaction,
+	InteractionExtractor,
+	Message,
+	MessageComponentType,
+} from 'discord.js';
+
+import { Constants, InteractionCollector } from 'discord.js'; // eslint-disable-line no-duplicate-imports
+
+/*
+  Most of this code is adapted from TextBasedChannel#awaitMessageComponent
+  to support a message of type APIMessage, which Message#awaitMessageComponent doesn't accept...
+
+  Had to look at discord.js typings to figure it out but it works :c
+*/
 
 export async function awaitComponent<
-  T extends MessageComponentType | keyof typeof Constants.MessageComponentTypes
+	T extends MessageComponentType | keyof typeof Constants.MessageComponentTypes = 'ACTION_ROW'
 >(
-  client: Client,
-  message: Message | APIMessage,
-  options: AwaitMessageCollectorOptionsParams<T> = {}
+	client: Client,
+	message: Message | APIMessage,
+	options: AwaitMessageCollectorOptionsParams<T> = {}
 ): Promise<InteractionExtractor<T> | undefined> {
-  // Adapted from https://github.com/discordjs/discord.js/blob/stable/src/structures/interfaces/TextBasedChannel.js#L246
-  return new Promise((resolve, reject) => {
-    // @ts-ignore
-    const collector = new InteractionCollector(client, {
-      ...options,
-      interactionType: Constants.InteractionTypes.MESSAGE_COMPONENT,
-      message,
-      max: 1,
-    });
+	return new Promise((resolve, reject) => {
+		const collector = new InteractionCollector(client, {
+			...(options as CollectorFilter<[Interaction<'cached'>]>),
+			message,
+			max: 1,
+			interactionType: Constants.InteractionTypes.MESSAGE_COMPONENT,
+		});
 
-    collector.once("end", (interactions, reason) => {
-      const interaction = interactions.first();
-      if (interaction) resolve(interaction as InteractionExtractor<T>);
-      else reject(new Error(reason));
-    });
-  });
+		collector.once('end', (interactions, reason) => {
+			const interaction = interactions.first();
+			if (interaction) resolve(interaction as InteractionExtractor<T>);
+			else reject(new Error(reason));
+		});
+	});
 }
