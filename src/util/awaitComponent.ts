9bc1cfd61a7e8776e4ae6c9731d1c22f6b5fdb98
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2021-12-27 17:57:13 -0800

    chore: use proper typings from lib

--- src/util/awaitComponent.ts
+++ src/util/awaitComponent.ts
@@ -1,75 +1,15 @@
 import type { APIMessage } from 'discord-api-types/v9';
 import type {
 	AwaitMessageCollectorOptionsParams,
-	ButtonInteraction,
 	Client,
 	CollectorFilter,
 	Interaction,
-	InteractionCollectorOptions,
+	MappedInteractionTypes,
 	Message,
-	MessageComponentInteraction,
 	MessageComponentType,
-	SelectMenuInteraction,
 } from 'discord.js';
 
 import { Constants, InteractionCollector } from 'discord.js'; // eslint-disable-line no-duplicate-imports
-import { MessageComponentTypes } from 'discord.js/typings/enums';
-
-// THANK GOD THEY REMOVED ALL THESE TYPES :)
-
-type TaggedUnion<T, K extends keyof T, V extends T[K]> = T extends Record<K, V>
-	? T
-	: T extends Record<K, infer U>
-	? V extends U
-		? T
-		: never
-	: never;
-
-type InteractionCollectorOptionsResolvable<Cached = boolean> =
-	| MessageInteractionCollectorOptions<Cached>
-	| SelectMenuInteractionCollectorOptions<Cached>
-	| ButtonInteractionCollectorOptions<Cached>;
-
-type MessageComponentCollectorOptions<T extends MessageComponentInteraction> = Omit<
-	InteractionCollectorOptions<T>,
-	'channel' | 'message' | 'guild' | 'interactionType'
->;
-
-interface ButtonInteractionCollectorOptions<Cached = boolean>
-	extends MessageComponentCollectorOptions<Cached extends true ? ButtonInteraction<'cached'> : ButtonInteraction> {
-	componentType: 'BUTTON' | MessageComponentTypes.BUTTON;
-}
-
-interface SelectMenuInteractionCollectorOptions<Cached = boolean>
-	extends MessageComponentCollectorOptions<
-		Cached extends true ? SelectMenuInteraction<'cached'> : SelectMenuInteraction
-	> {
-	componentType: 'SELECT_MENU' | MessageComponentTypes.SELECT_MENU;
-}
-
-interface MessageInteractionCollectorOptions<Cached = boolean>
-	extends MessageComponentCollectorOptions<
-		Cached extends true ? MessageComponentInteraction<'cached'> : MessageComponentInteraction
-	> {
-	componentType: 'ACTION_ROW' | MessageComponentTypes.ACTION_ROW;
-}
-type CollectorOptionsTypeResolver<U extends InteractionCollectorOptionsResolvable<Cached>, Cached = boolean> = {
-	readonly [T in U['componentType']]: TaggedUnion<U, 'componentType', T>;
-};
-
-type MappedInteractionCollectorOptions<Cached = boolean> = CollectorOptionsTypeResolver<
-	InteractionCollectorOptionsResolvable<Cached>,
-	Cached
->;
-
-type InteractionExtractor<
-	T extends MessageComponentType | MessageComponentTypes | undefined,
-	C extends boolean = false
-> = T extends MessageComponentType | MessageComponentTypes
-	? MappedInteractionCollectorOptions<C>[T] extends InteractionCollectorOptions<infer Item>
-		? Item
-		: never
-	: MessageComponentInteraction;
 
 export async function awaitComponent<
 	T extends MessageComponentType | keyof typeof Constants.MessageComponentTypes = 'ACTION_ROW'
@@ -77,7 +17,7 @@ export async function awaitComponent<
 	client: Client,
 	message: Message | APIMessage,
 	options: AwaitMessageCollectorOptionsParams<T> = {}
-): Promise<InteractionExtractor<T> | undefined> {
+): Promise<MappedInteractionTypes[T] | undefined> {
 	return new Promise((resolve, reject) => {
 		const collector = new InteractionCollector(client, {
 			...(options as CollectorFilter<[Interaction<'cached'>]>),
@@ -89,7 +29,7 @@ export async function awaitComponent<
 
 		collector.once('end', (interactions, reason) => {
 			const interaction = interactions.first();
-			if (interaction) resolve(interaction as InteractionExtractor<T>);
+			if (interaction) resolve(interaction as MappedInteractionTypes[T]);
 			else reject(new Error(reason));
 		});
 	});
