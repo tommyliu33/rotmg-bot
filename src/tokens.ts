d4877c3e4b6d1946d1d09b039373e498daec87c7
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2022-01-01 15:07:07 -0800

    chore: prettier

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,5 +1,5 @@
-export const kClient = Symbol("Client");
-export const kRedis = Symbol("Redis");
-export const kPrisma = Symbol("Prisma");
-export const kRaids = Symbol("Raid");
-export const kCommands = Symbol("Commands");
+export const kClient = Symbol('Client');
+export const kRedis = Symbol('Redis');
+export const kPrisma = Symbol('Prisma');
+export const kRaids = Symbol('Raids');
+export const kCommands = Symbol('Commands');
