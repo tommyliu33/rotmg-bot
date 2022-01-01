f19a4b6fc2739269cb67839c79e4aad1994dda79
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2022-01-01 15:05:42 -0800

    refactor: remove redundant await

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,30 +1,27 @@
-import type { Bot, Event } from "@struct";
+import { Bot, Event } from '@struct';
 
-import { getInteractions } from "@util";
-import { inject, injectable } from "tsyringe";
-import { logger } from "../logger";
-import { kClient } from "../tokens";
+import { getInteractions } from '@util';
+import { inject, injectable } from 'tsyringe';
+import { logger } from '../logger';
+import { kClient } from '../tokens';
 
 @injectable()
 export default class implements Event {
-  public name = "ready";
+	public name = 'ready';
 
-  public constructor(@inject(kClient) public readonly client: Bot) {}
+	public constructor(@inject(kClient) public readonly client: Bot) {}
 
-  public async execute() {
-    logger.info("Logged in");
+	public async execute() {
+		logger.info('Logged in');
+		if (process.argv.includes('--update')) {
+			const app = await this.client.application?.fetch();
+			const guilds = await this.client.guilds.fetch();
 
-    if (process.argv.includes("--update")) {
-      const app = await this.client.application?.fetch();
-      const guilds = await this.client.guilds.fetch();
+			for (const guild of guilds.values()) {
+				await app?.commands.set(getInteractions(), guild.id);
 
-      for (const guild of guilds.values()) {
-        await app?.commands.set(await getInteractions(), guild.id);
-
-        logger.info(
-          `Updated application commands for ${guild.name} (${guild.id})`
-        );
-      }
-    }
-  }
+				logger.info(`Updated (/) commands for ${guild.name} (${guild.id})`);
+			}
+		}
+	}
 }
