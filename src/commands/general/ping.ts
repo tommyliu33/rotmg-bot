c4493c9847b989a8aab1efad2208be51cb66a0ba
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2022-01-01 15:06:46 -0800

    chore: prettier

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -1,11 +1,11 @@
-import type { CommandInteraction } from "discord.js";
-import { Command } from "@struct";
+import type { CommandInteraction } from 'discord.js';
+import { Command } from '@struct';
 
 export default class implements Command {
-  public name = "ping";
-  public description = "pong.";
+	public name = 'ping';
+	public description = 'pong.';
 
-  public execute(interaction: CommandInteraction) {
-    void interaction.reply("pong.");
-  }
+	public execute(interaction: CommandInteraction) {
+		void interaction.reply('pong');
+	}
 }
