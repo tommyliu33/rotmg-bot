bf43ec079ac1c650e113e827d86583e0d8469273
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2022-01-01 15:06:19 -0800

    chore: prettier

--- src/util/getInteractions.ts
+++ src/util/getInteractions.ts
@@ -1,21 +1,16 @@
-import type { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v9";
-import type { Command } from "../struct/Command";
+import type { RESTPostAPIApplicationCommandsJSONBody } from 'discord-api-types/v9';
+import type { Command } from '../struct/Command';
 
-import { container } from "tsyringe";
-import { kCommands } from "../tokens";
+import { container } from 'tsyringe';
+import { kCommands } from '../tokens';
 
 export function getInteractions(): RESTPostAPIApplicationCommandsJSONBody[] {
-  const commands = container.resolve<Map<string, Command>>(kCommands);
+	const commands = container.resolve<Map<string, Command>>(kCommands);
 
-  const interactions = [];
-  for (const {
-    name,
-    description = name,
-    options = [],
-    default_permission = true,
-  } of commands.values()) {
-    interactions.push({ name, description, options, default_permission });
-  }
+	const interactions = [];
+	for (const { name, description = name, options = [], default_permission = true } of commands.values()) {
+		interactions.push({ name, description, options, default_permission });
+	}
 
-  return interactions;
+	return interactions;
 }
