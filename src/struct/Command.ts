1ba8639351b88ff102c174fe88f1843b30652576
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2022-01-01 15:05:17 -0800

    refactor: update function signature

--- src/struct/Command.ts
+++ src/struct/Command.ts
@@ -1,11 +1,11 @@
-import type { BaseCommandInteraction } from "discord.js";
-import type { APIApplicationCommandOption } from "discord-api-types/v9";
+import type { CommandInteraction } from 'discord.js';
+import type { APIApplicationCommandOption } from 'discord-api-types/v9';
 
 export interface Command {
-  name: string;
-  description?: string;
-  default_permission?: boolean;
-  options?: APIApplicationCommandOption[];
+	name: string;
+	description?: string;
+	default_permission?: boolean;
+	options?: APIApplicationCommandOption[];
 
-  execute(interaction: BaseCommandInteraction): void | Promise<void>;
+	execute(interaction: CommandInteraction): Promise<void> | void;
 }
