9663451e39356b2a55f98407f16d03d94c75e063
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2022-01-01 19:40:08 -0800

    chore: type properties for getGuildSetting

--- src/functions/settings/getGuildSetting.ts
+++ src/functions/settings/getGuildSetting.ts
@@ -1,17 +1,19 @@
-import type { PrismaClient } from '.prisma/client';
-import type { Snowflake } from 'discord.js';
 import { container } from 'tsyringe';
 import { kPrisma } from '../../tokens';
 
-export enum SettingsKey {
+import type { PrismaClient } from '@prisma/client';
+
+enum SettingsKey {
+	GuildId = 'guild_id',
+
 	AfkCheck = 'afk_check_channel_id',
 	VetAfkCheck = 'vet_afk_check_channel_id',
 
 	MainSection = 'main_section_id',
 	VetSection = 'veteran_section_id',
 
-	MainUserRole = 'verified_role_id',
-	VetUserRole = 'veteran_role_id',
+	VerifiedRole = 'verified_role_id',
+	VeteranRole = 'veteran_role_id',
 
 	RaidLeaderRole = 'raid_leader_role_id',
 	VetRaidLeaderRole = 'vet_raid_leader_role_id',
@@ -36,71 +38,100 @@ export enum SettingsKey {
 	FungalCavern = 'fungal',
 }
 
-export async function getGuildSetting(guildId: Snowflake, key: SettingsKey): Promise<any> {
+interface GuildSettings {
+	GuildId: string;
+
+	AfkCheck: string;
+	VetAfkCheck: string;
+
+	LogChannel: string;
+
+	VerifiedRole: string;
+	RaidLeaderRole: string;
+	VeteranRole: string;
+	VetRaidLeaderRole: string;
+	SuspendRole: string;
+
+	MainSection: string;
+	VetSection: string;
+
+	MainSectionVoiceChannels: string[];
+	VetSectionVoiceChannels: string[];
+
+	MainSectionRequirements: {
+		private_location: boolean;
+		rank: number;
+	};
+	VeteranSectionRequirements: {
+		o3: 0;
+		void: 0;
+		shatters: 0;
+		cult: 0;
+		nest: 0;
+		fungal: 0;
+	};
+
+	MainVerificationButton: string;
+	VeteranVerificationButton: string;
+}
+
+export async function getGuildSetting<Setting extends keyof GuildSettings>(
+	guildId: string,
+	key: Setting
+): Promise<GuildSettings[Setting]> {
 	const prisma = container.resolve<PrismaClient>(kPrisma);
 
-	let data = await prisma.guilds
-		.findFirst({
-			where: {
-				id_: guildId,
-			},
-		})
-		.catch(() => {
-			return undefined;
-		});
-
-	if (!data) {
-		data = await prisma.guilds.create({
-			data: {
-				id_: guildId,
-
-				afk_check_channel_id: '',
-				vet_afk_check_channel_id: '',
-
-				main_section_id: '',
-				veteran_section_id: '',
-
-				verified_role_id: '',
-				veteran_role_id: '',
-
-				raid_leader_role_id: '',
-				vet_raid_leader_role_id: '',
-
-				log_channel_id: '',
-				suspend_role_id: '',
-
-				main_section_voice_channel_ids: [],
-				veteran_section_voice_channel_ids: [],
-
-				private_location: true,
-				rank: 0,
-
-				main_verification_button_id: '',
-				veteran_verification_button_id: '',
-
-				o3: 0,
-				void: 0,
-				cult: 0,
-				fungal: 0,
-				shatters: 0,
-				nest: 0,
-
-				main_section_requirements: {
-					private_location: false,
-					rank: 0,
+	const data =
+		(await prisma.guilds
+			.findFirst({
+				where: {
+					guild_id: guildId,
 				},
+			}) ?? (await setDefaultGuildSettings(prisma, guildId));
+	const rawKey = Reflect.get(SettingsKey, key) as Setting;
+	return Reflect.get(data, rawKey);
+}
 
-				veteran_section_requirements: {
-					o3: 0,
-					void: 0,
-					shatters: 0,
-					cult: 0,
-					nest: 0,
-					fungal: 0,
-				},
-			},
-		});
-	}
+async function setDefaultGuildSettings(prisma: PrismaClient, guildId: string) {
+	const defaultSettings = {
+		guild_id: guildId,
 
-	return data[key];
+		afk_check_channel_id: '',
+		vet_afk_check_channel_id: '',
+
+		main_section_id: '',
+		veteran_section_id: '',
+
+		verified_role_id: '',
+		veteran_role_id: '',
+
+		raid_leader_role_id: '',
+		vet_raid_leader_role_id: '',
+
+		log_channel_id: '',
+		suspend_role_id: '',
+
+		main_section_voice_channel_ids: [],
+		veteran_section_voice_channel_ids: [],
+
+		main_verification_button_id: '',
+		veteran_verification_button_id: '',
+
+		main_section_requirements: {
+			private_location: false,
+			rank: 0,
+		},
+
+		veteran_section_requirements: {
+			o3: 0,
+			void: 0,
+			shatters: 0,
+			cult: 0,
+			nest: 0,
+			fungal: 0,
+		},
+	};
+
+	await prisma.guilds.create({ data: defaultSettings });
+	return defaultSettings;
 }
