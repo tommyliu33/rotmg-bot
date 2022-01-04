368b08a0ff2dc77902ba9562dea769a112aff6db
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2022-01-04 15:18:01 -0800

    fix(Utils): inVetChannel incorrect return value

--- src/util/inVetChannel.ts
+++ src/util/inVetChannel.ts
@@ -1,20 +1,6 @@
-import { container } from 'tsyringe';
-import { kPrisma } from '../tokens';
+import { getGuildSetting } from 'src/functions';
 
-import type { PrismaClient } from '@prisma/client';
-
-// TODO: this does nothing lol
 export async function inVetChannel(guildId: string, channelId: string): Promise<boolean> {
-	const prisma = container.resolve<PrismaClient>(kPrisma);
-
-	const data = await prisma.guilds.findFirst({
-		where: {
-			guild_id: guildId,
-		},
-		select: {
-			veteran_section_voice_channel_ids: true,
-		},
-	});
-
-	return data?.veteran_section_voice_channel_ids.indexOf(channelId) !== -1; // eslint-disable-line @typescript-eslint/prefer-includes
+	const vetChannelId = await getGuildSetting(guildId, 'VetAfkCheck');
+	return channelId === vetChannelId;
 }
