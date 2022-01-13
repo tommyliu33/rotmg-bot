b5e6f35c0d73343240be7a5fec881a3e5db6aac3
tommyliu33 <86510873+tommyliu33@users.noreply.github.com>
2022-01-13 12:11:08 -0800

    fix: move client to main

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import 'reflect-metadata';
-
 import 'dotenv/config';
 
-import { Bot, RaidManager } from './struct';
-import Prisma from '@prisma/client'; // no es module support :[
+import { Client, Options } from 'discord.js';
+import { RaidManager, Event, Command } from './struct';
+import Prisma from '@prisma/client';
 import { logger } from './logger';
-import type { Event, Command } from './struct'; // eslint-disable-line no-duplicate-imports
 
 import readdirp from 'readdirp';
 import Bree from 'bree';
@@ -15,15 +14,36 @@ import { kClient, kCommands, kPrisma, kRedis, kRaids, kBree } from './tokens';
 
 import Redis from 'ioredis';
 
+const client = new Client({
+	intents: [
+		'GUILDS',
+		'GUILD_MESSAGES',
+		'GUILD_MEMBERS',
+		'GUILD_VOICE_STATES',
+		'GUILD_MESSAGE_REACTIONS',
+
+		'DIRECT_MESSAGES',
+		'DIRECT_MESSAGE_REACTIONS',
+	],
+	partials: ['CHANNEL', 'REACTION', 'GUILD_MEMBER', 'MESSAGE'],
+	makeCache: Options.cacheWithLimits({
+		GuildInviteManager: 0,
+		GuildStickerManager: 0,
+		PresenceManager: 0,
+		StageInstanceManager: 0,
+		ThreadManager: 0,
+		ThreadMemberManager: 0,
+	}),
+});
+
 const prisma = new Prisma.PrismaClient();
 await prisma.$connect();
 
 const redis = new Redis(process.env.REDIS_HOST);
-const client = new Bot();
 
 const commands = new Map<string, Command>();
 
-const bree = new Bree({ root: false });
+const bree = new Bree({ root: false, logger });
 
 container.register(kPrisma, { useValue: prisma });
 container.register(kRedis, { useValue: redis });
@@ -50,7 +70,6 @@ for await (const { fullPath } of readdirp('./events')) {
 	const event = container.resolve<Event>((await import(fullPath)).default);
 
 	if (event.emitter === 'raidManager') await event.execute();
-
 	if (event.emitter === 'client') {
 		client.on(event.name, async (...args: unknown[]) => event.execute(...args));
 	}
