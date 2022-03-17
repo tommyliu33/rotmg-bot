f48c441624a15f001f9b802458ad4716220d917b
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-03-17 14:04:37 -0700

    feat: delete control panel pin msg

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,13 @@
+import type { Event } from '../struct/Event';
+import { Events, Message } from 'discord.js';
+import { MessageType } from 'discord-api-types/v10';
+
+export default class implements Event {
+	public name = 'Message creation';
+	public event = Events.MessageCreate;
+
+	public run(msg: Message) {
+		if (msg.author.id === msg.client.user!.id && msg.type === MessageType.ChannelPinnedMessage)
+			void msg.delete().catch(() => undefined);
+	}
+}
