7c63c183b49a415ffc1175621bcc28ecb67a0cd4
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-03-25 21:04:04 -0700

    feat: support for modularized sub-commands

--- src/util/commands.ts
+++ src/util/commands.ts
@@ -1,9 +1,6 @@
 import readdirp from 'readdirp';
-
 import { logger } from './logger';
-
 import { container } from 'tsyringe';
-import type { Constructor } from '@sapphire/utilities';
 import type { Command } from '../struct/Command';
 
 export async function loadCommands(directory: string) {
@@ -14,9 +11,18 @@ export async function loadCommands(directory: string) {
 		const commandMod = (await import(dir.fullPath)) as { default: Constructor<Command> };
 		const command = container.resolve(commandMod.default);
 
-		commands.set(command.name, command);
-		logger.info(`Registered command: ${command.name}`);
+		if (commands.has(command.name)) {
+			commands.set(command.name, {
+				...command,
+				options: [...commands.get(command.name)!.options!, ...command.options!],
+			});
+		} else {
+			commands.set(command.name, command);
+			logger.info(`Registered command: ${command.name}`);
+		}
 	}
 
 	return commands;
 }
+
+type Constructor<T> = new (...args: any[]) => T;
