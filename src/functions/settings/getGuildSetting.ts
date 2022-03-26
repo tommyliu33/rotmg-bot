949695d5fee7d76fcf2eea21278070cb7afe4d47
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-03-26 10:10:13 -0700

    revert(settings): sub-key support for getGuildSetting

--- src/functions/settings/getGuildSetting.ts
+++ src/functions/settings/getGuildSetting.ts
@@ -1,28 +1,19 @@
 import { guilds } from '../../util/mongo';
 
-export async function getGuildSetting<K extends keyof Settings>(
-	guildId: string,
-	key: K,
-	subKey?: keyof Settings['main']
-) {
+export async function getGuildSetting<K extends keyof Settings>(guildId: string, key: K) {
 	const data = await guilds.findOne({ guild_id: guildId });
 
 	const data_ = data![key];
-
-	if (!subKey) {
-		return {
-			categoryId: data_.category_id,
-			afkCheckChannelId: data_.afk_check_channel_id,
-			verificationChannelId: data_.verification_channel_id,
-			controlPanelChannelId: data_.control_panel_channel_id,
-			voiceChannelIds: data_.voice_channel_ids,
-			verificationRequirements: data_.verification_requirements,
-			userRole: data_.user_role,
-			leaderRole: data_.leader_role,
-		};
-	}
-
-	return data_[subKey];
+	return {
+		categoryId: data_.category_id,
+		afkCheckChannelId: data_.afk_check_channel_id,
+		verificationChannelId: data_.verification_channel_id,
+		controlPanelChannelId: data_.control_panel_channel_id,
+		voiceChannelIds: data_.voice_channel_ids,
+		verificationRequirements: data_.verification_requirements,
+		userRole: data_.user_role,
+		leaderRole: data_.leader_role,
+	};
 }
 
 export interface Settings {
