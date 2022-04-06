a104697505b08ca41df8d46b91ffc634470c43bd
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-04-06 15:23:04 -0700

    chore: fix deploy script not being called

--- src/util/deploy.ts
+++ src/util/deploy.ts
@@ -10,26 +10,17 @@ import type { Command } from '#struct/Command';
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN!);
 const commands = container.resolve<Map<string, Command>>(kCommands);
 
-async function deploy() {
-	if (commands.size === 0) {
-		logger.warn('No slash commands to refresh');
-		return;
-	}
+const array = [...commands.values()];
+const payload = array.reduce<ApplicationCommand[]>((array, command) => {
+	array.push({ name: command.name, description: command.description, options: command.options ?? [] });
+	return array;
+}, []);
 
-	const array = [...commands.values()];
-	const payload = array.reduce<ApplicationCommand[]>((array, command) => {
-		array.push({ name: command.name, description: command.description, options: command.options ?? [] });
-		return array;
-	}, []);
-
-	try {
-		await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!), { body: payload });
-		logger.info('Sucessfully refreshed slash commands for guild');
-	} catch (e) {
-		logger.error(e, 'Failed to refresh slash commands for guild');
-	}
+try {
+	await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!), { body: payload });
+	logger.info('Sucessfully refreshed slash commands for guild');
+} catch (e) {
+	logger.error(e, 'Failed to refresh slash commands for guild');
 }
-
-void deploy();
 
 type ApplicationCommand = Pick<APIApplicationCommand, 'name' | 'description' | 'options'>;
