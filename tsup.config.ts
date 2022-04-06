9104463eb91b899303919bcf180914c195d6d6df
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-04-05 22:30:32 -0700

    chore: tsup / tsconfig path support

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig((options) => ({
+	clean: true,
+	dts: false,
+	entry: ['src/**/*.ts'],
+	format: ['esm'],
+	minify: false,
+	watch: options.watch,
+	skipNodeModulesBundle: true,
+	sourcemap: true,
+	target: 'esnext',
+	tsconfig: 'tsconfig.json',
+	bundle: false,
+	shims: false,
+	keepNames: true,
+	splitting: false,
+}));
