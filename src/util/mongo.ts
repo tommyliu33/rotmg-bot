694ae472352e50b2b146f01e3d193e62857af1eb
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-04-12 15:29:24 -0700

    chore: setup users collection

--- src/util/mongo.ts
+++ src/util/mongo.ts
@@ -5,9 +5,9 @@ const client = new MongoClient(process.env.CONNECTION_STRING!);
 const db = client.db('rotmg');
 
 export const guilds = db.collection<GuildDocument>('guilds');
+export const users = db.collection<UserDocument>('users');
 
-(async () => {
-	await client.connect();
-})();
+await client.connect();
 
 type GuildDocument = Settings & Document & { guild_id: string };
+type UserDocument = Document & { guild_id: string };
