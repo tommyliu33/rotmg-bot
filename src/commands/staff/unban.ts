432fb2fc4c1cb923634ce6b335f4fdf279d4693b
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-05-06 10:06:36 -0700

    feat(commands): unban

---
+++ src/commands/staff/unban.ts
@@ -0,0 +1,45 @@
+import type { Command } from '#struct/Command';
+import type { ChatInputCommandInteraction } from 'discord.js';
+
+import { createCase, ModLogAction } from '#functions/moderation/createCase';
+
+export default class implements Command {
+	public name = 'unban';
+	public description = 'Unban a user';
+	public options = [
+		{
+			name: 'user',
+			description: 'The user for this action',
+			type: 6,
+			required: true,
+		},
+		{
+			name: 'reason',
+			description: 'The reason for this action',
+			type: 3,
+			required: false,
+		},
+	];
+
+	public async run(interaction: ChatInputCommandInteraction<'cached'>) {
+		await interaction.deferReply({ ephemeral: true, fetchReply: true });
+
+		const target = interaction.options.getUser('user', true);
+		const reason = interaction.options.getString('reason', false) ?? 'No reason provided';
+
+		try {
+			await interaction.guild.bans.fetch(target.id);
+		} catch {
+			throw new Error('This user is not banned.');
+		}
+
+		await createCase({
+			action: ModLogAction.Unban,
+			moderator: interaction.member,
+			target,
+			reason,
+		});
+
+		await interaction.editReply(`${target.tag} was unbanned.`);
+	}
+}
