98e95b13fa11b2f44a6ea10a3cfa8f960980dbcf
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-07-16 20:35:35 -0700

    feat(commands): notes viewing

--- src/commands/staff/notes.ts
+++ src/commands/staff/notes.ts
@@ -2,6 +2,8 @@ import type { ChatInputCommandInteraction } from 'discord.js';
 import type { Command } from '#struct/Command';
 
 import { users, createUser } from '#util/mongo';
+import { EmbedBuilder } from '@discordjs/builders';
+import { paginate } from '#functions/paginate';
 
 export default class implements Command {
 	public async run(interaction: ChatInputCommandInteraction<'cached'>) {
@@ -47,6 +49,22 @@ export default class implements Command {
 	}
 
 	private async view(interaction: ChatInputCommandInteraction<'cached'>) {
-		await interaction.reply('view');
+		await interaction.deferReply({ ephemeral: true, fetchReply: true });
+		const u = interaction.options.getUser('user', true);
+
+		const doc = await createUser(u.id);
+		const guild = doc.guilds.find((user) => user.guild_id === interaction.guildId && user.notes?.length);
+		if (guild?.notes) {
+			const embeds = [];
+			for (const note of guild.notes) {
+				const embed = new EmbedBuilder()
+					.setAuthor({ name: note.author })
+					.setDescription(note.message)
+					.setTimestamp(note.timestamp);
+				embeds.push(embed);
+			}
+
+			await paginate(interaction, embeds);
+		}
 	}
 }
