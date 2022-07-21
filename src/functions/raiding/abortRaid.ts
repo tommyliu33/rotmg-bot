d78ed00dcb6618f0b63b161adf17d87c627f551a
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-07-21 12:54:44 -0700

    fix: import spelling

--- src/functions/raiding/abortRaid.ts
+++ src/functions/raiding/abortRaid.ts
@@ -1,14 +1,13 @@
-emport type { Raid } from './startRaid';
+import type { Raid } from './startRaid';
 import type { RaidManager } from '../../components/RaidManager';
 
 export function abortRaid(this: { raidManager: RaidManager }, raidInfo?: Raid) {
 	if (raidInfo) {
 		const raidKey = `${raidInfo.guildId}-${raidInfo.memberId}`;
-		
+
 		// TODO: this should update the raidInfo message and control panel
-		
+
 		return this.raidManager.raids.delete(raidKey);
-		
 	}
 
 	return false;
