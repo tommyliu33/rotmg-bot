5fb29cc378ba0e1682df8ca739fff037b8bd74ff
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-07-22 21:35:21 -0700

    feat: raid aborting

--- src/commands/abort.ts
+++ src/commands/abort.ts
@@ -5,12 +5,14 @@ import type { CommandEntity } from '../components/CommandEntity';
 
 import { CommandManager } from '../components/CommandManager';
 import { RaidManager } from '../components/RaidManager';
+import { Discord } from '#components/Discord';
 import { abortRaid } from '#functions/raiding/abortRaid';
 
 export default class implements CommandEntity {
 	public name = 'commands:abort';
 	public parent = CommandManager;
 
+	@Inject(Discord) private readonly discord!: Discord;
 	@Inject(RaidManager) private readonly raidManager!: RaidManager;
 
 	public async run(interaction: ChatInputCommandInteraction<'cached'>) {
@@ -18,8 +20,9 @@ export default class implements CommandEntity {
 
 		const raid = this.raidManager.raids.get(`${interaction.guildId}-${interaction.member.id}`);
 		if (raid) {
-			await abortRaid.call({ raidManager: this.raidManager }, raid);
+			await abortRaid.call({ raidManager: this.raidManager, discord: this.discord }, raid);
 			await interaction.editReply({ content: 'Raid aborted.' });
+			return;
 		}
 
 		await interaction.editReply('No raid found.');
--- src/functions/raiding/abortRaid.ts
+++ src/functions/raiding/abortRaid.ts
@@ -1,10 +1,34 @@
+import { type TextChannel, type ThreadChannel, EmbedBuilder } from 'discord.js';
 import type { Raid } from './startRaid';
+import type { Discord } from '#components/Discord';
 import type { RaidManager } from '#components/RaidManager';
 
-export function abortRaid(this: { raidManager: RaidManager }, raidInfo: Raid<false>) {
+export function abortRaid(this: { discord: Discord; raidManager: RaidManager }, raidInfo: Raid) {
 	const raidKey = `${raidInfo.guildId}-${raidInfo.memberId}`;
-	
-	// TODO: this should update the raidInfo message and control panel
 
-	return this.raidManager.raids.delete(raidKey);
+	// TODO: this should update the raidInfo message and control panel
+	if (this.raidManager.raids.get(raidKey)) {
+		const guild = this.discord.client.guilds.cache.get(raidInfo.guildId)!;
+		const statusChannel = guild.channels.cache.get(raidInfo.textChannelId) as TextChannel;
+		const controlPanelThread = guild.channels.cache.get(raidInfo.controlPanelThreadId) as ThreadChannel;
+
+		const statusChannelMessage = statusChannel.messages.cache.get(raidInfo.mainMessageId);
+
+		const member = guild.members.cache.get(raidInfo.memberId)!;
+
+		if (statusChannelMessage) {
+			const firstEmbed = new EmbedBuilder()
+				.setColor('DarkRed')
+				.setAuthor({
+					name: `Aborted by ${member.displayName}`,
+					iconURL: member.displayAvatarURL(),
+				})
+				.setTimestamp();
+
+			void statusChannelMessage.edit({ content: ' ', components: [], embeds: [firstEmbed] });
+		}
+
+		void controlPanelThread.delete(`Raid aborted by ${member.displayName} (${member.id})`);
+		return this.raidManager.raids.delete(raidKey);
+	}
 }
